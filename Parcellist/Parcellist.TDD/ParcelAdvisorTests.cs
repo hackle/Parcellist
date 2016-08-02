3acf8b85e0994f8f15c2ab4bf7a3abfa5a746509
Hackle Wayne <hacklew@hotmail.com>
2016-08-02 21:44:07 +1200

    Can shuffle

--- Parcellist/Parcellist.TDD/ParcelAdvisorTests.cs
+++ Parcellist/Parcellist.TDD/ParcelAdvisorTests.cs
@@ -14,9 +14,11 @@ namespace Parcellist.TDD
         [InlineAutoData(210, 280, 130, 50)]
         [InlineAutoData(280, 390, 180, 75)]
         [InlineAutoData(380, 550, 200, 85)]
-        public void Can_advise_tight_fits(int d1, int d2, int d3, int cost, ParcelAdvisor advisor)
+        public void Can_advise_tight_fits_regardless_of_order_of_dimensions(int d1, int d2, int d3, int cost, ParcelAdvisor advisor)
         {
-            var parcel = new Parcel(d1, d2, d3, 5.2M);
+            var dimensions = new[] { d1, d2, d3 }.OrderBy(d => Guid.NewGuid()).ToArray();
+
+            var parcel = new Parcel(dimensions[0], dimensions[1], dimensions[2], 5.2M);
 
             var offer = advisor.Advise(parcel);
 
