a413103af9d33c2593ed07b6fef7149067517e64
Hackle Wayne <hacklew@hotmail.com>
2016-08-02 20:49:01 +1200

    Have projects

---
+++ Parcellist/Parcellist.TDD/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture.NUnit3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcellist.TDD
+{
+    [TestFixture]
+    public class Class1
+    {
+    }
+}
