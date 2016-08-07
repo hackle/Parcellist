98e2e6fb0cb6d2bb8c097a274b5ed8f38e04a6bf
Hackle Wayne <hacklew@hotmail.com>
2016-08-07 20:08:19 +1200

    Fully tested Package

--- Farcel/Farcel/Package.fs
+++ Farcel/Farcel/Package.fs
@@ -28,7 +28,7 @@ module SizeTests =
     [<InlineData(15000)>]
     let ``Positive numbers result in success`` (size: int) =
         let actual = Size.create size
-        
+
         match actual with
         | Success s ->         
             let result = Size.value s
@@ -90,6 +90,33 @@ module Weight =
             then Success (Weight w)
             else Failure "Weight is not valid"
 
+    let value w =
+        let (Weight v) = w
+        v
+
+module WeightTests =
+    open Xunit
+    open Swensen.Unquote
+
+    [<Theory>]
+    [<InlineData(1)>]
+    [<InlineData(0)>]
+    let ``Non-negative weight is allowed`` (w:decimal) =
+        let weight = Weight.create w
+        match weight with
+        | Success s ->
+            let actual = Weight.value s
+            test <@ actual = w @>
+        | Failure _ -> Assert.True(false, "Should not fail")
+
+    [<Theory>]
+    [<InlineData(-1)>]
+    let ``Negative weight is not cool`` (expected:decimal) =
+        let weight = Weight.create expected
+        match weight with
+        | Success s -> Assert.True(false, "Should not succeed")
+        | Failure m -> test <@ m.Contains("not valid") @>
+
 module Package = 
     type T = 
             {
@@ -112,6 +139,29 @@ module Package =
             return Success { Name=n; Width=d1; Height=d2; Breadth=d3; Cost=c}
             }
 
+module PackageTests =
+    open Xunit
+    open Swensen.Unquote
+
+    [<Theory>]
+    [<InlineData(0, 1, 1, 1)>]
+    [<InlineData(1, 0, 1, 1)>]
+    [<InlineData(1, 1, 0, 1)>]
+    [<InlineData(1, 1, 1, -1)>]
+    let ``invalid input will result in invalid package`` (width: int) (height: int) (breadth: int) (cost: decimal) =
+        let actual = Package.create "any name" width height breadth cost
+        match actual with
+        | Success p -> Assert.True(false, "should not succeed")
+        | Failure m -> Assert.True(true)
+
+    [<Theory>]
+    [<InlineData(1, 1, 1, 1)>]
+    let ``Valid input will result in valid package`` (width: int) (height: int) (breadth: int) (cost: decimal) =
+        let actual = Package.create "any name" width height breadth cost
+        match actual with
+        | Success p -> Assert.True(true)
+        | Failure m -> Assert.True(false, "should not fail")
+
 module ItemToSend =
     type T = 
             {
@@ -131,3 +181,26 @@ module ItemToSend =
             let! w = Weight.create weight
             return Success {Width=d1;Height=d2;Breadth=d3;Weight=w}
             }
+
+module ItemToSendTests =
+    open Xunit
+    open Swensen.Unquote
+
+    [<Theory>]
+    [<InlineData(1, 1, 1, -1)>]
+    [<InlineData(1, 1, -1, 1)>]
+    [<InlineData(1, -1, 1, 1)>]
+    [<InlineData(-1, 1, 1, 1)>]
+    let ``Invalid data result in invalid item`` (width:int) (height:int) (breadth:int) (weight:decimal) =
+        let item = ItemToSend.create width height breadth weight
+        match item with
+        | Success _ -> Assert.True(false, "should not succeed")
+        | Failure _ -> Assert.True(true)
+
+    [<Theory>]
+    [<InlineData(1, 1, 1, 1)>]
+    let ``Valid data result in valid item`` (width:int) (height:int) (breadth:int) (weight:decimal) =
+        let item = ItemToSend.create width height breadth weight
+        match item with
+        | Success _ -> Assert.True(true)
+        | Failure _ -> Assert.True(false, "should not fail")
